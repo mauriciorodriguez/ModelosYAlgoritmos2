52f50a61c1c02d0ae88ac6e52fa2aaf7838e784e
Mauricio Ezequiel Rodriguez <rodriguez.mauricioe@gmail.com>
2016-06-04 17:42:38 -0300

    Proyecto base y Pool generico

---
+++ MYA2Juego/Assets/Scripts/Enemy/Asteroid.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Asteroid : MonoBehaviour
+{
+    public static int _count = 0;
+    public int hp;
+    public bool isOutOfScreen { private set; get; }
+
+    private Vector3 _position, _rotation;
+
+    public Asteroid()
+    {
+        gameObject.name = this.GetType().ToString() + ":" + (_count++);
+    }
+}
---
+++ MYA2Juego/Assets/Scripts/Pool/ObjectPool.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ObjectPool<T>
+{
+    public delegate T FactoryDelegate();
+
+    private Stack<T> _objects;
+    private FactoryDelegate _factory;
+
+    public ObjectPool(FactoryDelegate factory, int quantity = 10)
+    {
+        _objects = new Stack<T>();
+        _factory = factory;
+        if (quantity >= 0)
+        {
+            for (int i = 0; i < quantity; i++)
+            {
+                _objects.Push(Create());
+            }
+        }
+
+    }
+
+    public T GetObject()
+    {
+        if (_objects.Count > 0)
+        {
+            return _objects.Pop();
+        }
+        else
+        {
+            return Create();
+        }
+    }
+
+    public void PutBackObject(T obj)
+    {
+        _objects.Push(obj);
+    }
+
+    private T Create()
+    {
+        return _factory();
+    }
+}
